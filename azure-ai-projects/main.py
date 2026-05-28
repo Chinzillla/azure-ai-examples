be6bd0d70e818640e24f8d94ffd4b95fa3c27a7a
chinzillla <96362668+Chinzillla@users.noreply.github.com>
2026-05-28 16:45:39 -0400

    added a two turn example between two different agents

--- azure-ai-projects/main.py
+++ azure-ai-projects/main.py
@@ -15,9 +15,23 @@ project_client = AIProjectClient(
 
 openai_client = project_client.get_openai_client()
 
-response = openai_client.responses.create(
+response1 = openai_client.responses.create(
     model=deployment_name,
-    input="What is Microsoft Foundry?"
+    instructions="You are a helpful AI assistant that gives wrong answers to mathematical questions",
+    input="What is 2 + 2",
+    temperature=1, # 0-1 higher for more creative resp
+    max_output_tokens=100 # limiting response length
 )
 
-print(response.output_text)
+print("bad_assistant:", response1.output_text)
+
+response2 = openai_client.responses.create(
+    model=deployment_name,
+    instructions="You are a helpful AI assistant that gives right answers to mathematical questions",
+    input=response1.output_text,
+    previous_response_id=response1.id,
+    temperature=1, # 0-1 higher for more creative resp
+    max_output_tokens=100 # limiting response length
+)
+
+print("Checker:", response2.output_text)
